9dbf9345e4462e2480aba949200bd79ddbefdd2e
Karol <karolkania@wp.pl>
2023-04-26 10:49:51 +0200

    Initial commit

---
+++ src/main/java/myprojects/insurancepoliciesmanagerbykrollo/InsurancePoliciesManagerByKRolloApplication.java
@@ -0,0 +1,13 @@
+package myprojects.insurancepoliciesmanagerbykrollo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsurancePoliciesManagerByKRolloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InsurancePoliciesManagerByKRolloApplication.class, args);
+	}
+
+}
---
+++ src/test/java/myprojects/insurancepoliciesmanagerbykrollo/InsurancePoliciesManagerByKRolloApplicationTests.java
@@ -0,0 +1,13 @@
+package myprojects.insurancepoliciesmanagerbykrollo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InsurancePoliciesManagerByKRolloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
